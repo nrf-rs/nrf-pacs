2163d8619fcfc9f0b343836a951d5659658cc0a6
Jonas Schievink <jonasschievink@gmail.com>
2021-08-06 22:16:50 +0200

    Check more things in CI

--- xtask/tests/ci.rs
+++ xtask/tests/ci.rs
@@ -1,4 +1,4 @@
-use std::env;
+use std::{collections::HashMap, env};
 use xshell::cmd;
 
 fn main() {
@@ -7,13 +7,29 @@ fn main() {
 
     xtask::install_tools();
 
-    // TODO: regen and check that nothing was changed
+    // Regenerate PACs and check that nothing has changed.
+    let was_clean = cmd!("git status --porcelain").read().unwrap().is_empty();
+    xtask::generate();
+    let is_clean = cmd!("git status --porcelain").read().unwrap().is_empty();
+    if was_clean && !is_clean {
+        cmd!("git status").run().unwrap();
+        panic!("working directory not clean, run `cargo xtask generate` and push the result");
+    }
 
-    // Build-test every PAC.
+    // Test that every PAC builds for the intended target.
+    // We group them by target so that we can have Cargo build some of them in parallel.
+    // FIXME: With https://github.com/rust-lang/cargo/issues/8176 this could be a single invocation.
+    let mut target_map: HashMap<_, Vec<_>> = HashMap::new();
     for (pac, target) in xtask::PACS {
-        let toml_path = format!("pacs/{}-pac/Cargo.toml", pac);
-        cmd!("cargo build --manifest-path {toml_path} --target {target}")
-            .run()
-            .unwrap();
+        target_map.entry(target).or_default().push(pac);
+    }
+
+    for (target, pacs) in target_map {
+        let mut cmd = cmd!("cargo build --target {target}");
+        for pac in pacs {
+            let package = format!("{}-pac", pac);
+            cmd = cmd.args(&["-p", &package]);
+        }
+        cmd.run().unwrap();
     }
 }
