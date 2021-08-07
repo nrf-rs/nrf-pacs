e9e4af2edf32d4166a98206f8f18165ee415e7d1
Jonas Schievink <jonasschievink@gmail.com>
2021-08-07 04:23:42 +0200

    Extend `xtask publish` automation

--- xtask/tests/ci.rs
+++ xtask/tests/ci.rs
@@ -1,16 +1,15 @@
 use std::{collections::BTreeMap, env};
 use xshell::cmd;
+use xtask::is_git_clean;
 
 fn main() {
     // We execute from the `xtask` dir, so `cd ..` so that we can find everything.
     env::set_current_dir("..").unwrap();
 
-    xtask::install_tools();
-
     // Regenerate PACs and check that nothing has changed.
-    let was_clean = cmd!("git status --porcelain").read().unwrap().is_empty();
+    let was_clean = is_git_clean();
     xtask::generate();
-    let is_clean = cmd!("git status --porcelain").read().unwrap().is_empty();
+    let is_clean = is_git_clean();
     if !is_clean {
         cmd!("git status").run().unwrap();
         if was_clean {
